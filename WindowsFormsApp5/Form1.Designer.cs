8e5359cd4da9e2706370235c1d35ffdfd6ca9244
Emrah KARAMAN <ekaraman89@hotmail.com>
2018-10-26 01:31:37 +0300

    initial commit

--- WindowsFormsApp5/Form1.Designer.cs
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp5
+﻿namespace CrawlerWinForm
 {
     partial class Form1
     {
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rctxtResult = new System.Windows.Forms.RichTextBox();
+            this.btnParser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(591, 32);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Adrese Git";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(56, 34);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(529, 20);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "http://www.milliyet.com.tr/yazarlar/abbas-guclu/en-guclu-pasaport--2759676/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adres";
+            // 
+            // rctxtResult
+            // 
+            this.rctxtResult.Location = new System.Drawing.Point(12, 61);
+            this.rctxtResult.Name = "rctxtResult";
+            this.rctxtResult.Size = new System.Drawing.Size(654, 338);
+            this.rctxtResult.TabIndex = 3;
+            this.rctxtResult.Text = "";
+            // 
+            // btnParser
+            // 
+            this.btnParser.Location = new System.Drawing.Point(591, 405);
+            this.btnParser.Name = "btnParser";
+            this.btnParser.Size = new System.Drawing.Size(75, 23);
+            this.btnParser.TabIndex = 4;
+            this.btnParser.Text = "Temizle";
+            this.btnParser.UseVisualStyleBackColor = true;
+            this.btnParser.Click += new System.EventHandler(this.btnParser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(675, 437);
+            this.Controls.Add(this.btnParser);
+            this.Controls.Add(this.rctxtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rctxtResult;
+        private System.Windows.Forms.Button btnParser;
     }
 }
 
--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp5
+namespace CrawlerWinForm
 {
     public partial class Form1 : Form
     {
@@ -16,5 +12,64 @@ namespace WindowsFormsApp5
         {
             InitializeComponent();
         }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            rctxtResult.Text = GetResponse(txtUrl.Text);
+        }
+
+        private void btnParser_Click(object sender, EventArgs e)
+        {
+
+            string text = GetPlainTextFromHtml(rctxtResult.Text);
+
+            //http://www.milliyet.com.tr/aksal-yavuz/yusuf-ve-abdulkadir-e-destek-2766396-skorer-yazar-yazisi/
+            if (txtUrl.Text.IndexOf("http://www.milliyet.com.tr/yazarlar/") == -1)
+            {
+                text = Crop(text, "A+A-", "Yazarın tüm yazıları");
+            }
+            else //http://www.milliyet.com.tr/yazarlar/abbas-guclu/en-guclu-pasaport--2759676/
+            {
+                text = Crop(text, "Tüm Yazıları", "Yazarın Diğer Yazıları");
+            }
+
+            rctxtResult.Text = text;
+
+        }
+
+        private static string Crop(string text, string topCrop, string botomCrop)
+        {
+            int index = text.IndexOf(topCrop);
+            if (index > 0)
+                text = text.Remove(0, index + topCrop.Length);
+
+            index = text.IndexOf(botomCrop);
+            if (index > 0)
+                text = text.Remove(index).Trim();
+
+            return text;
+        }
+
+        private string GetResponse(string url)
+        {
+            WebRequest webRequest = HttpWebRequest.Create(url);
+            WebResponse webResponse = webRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(webResponse.GetResponseStream());
+
+            return streamReader.ReadToEnd();
+        }
+
+        private string GetPlainTextFromHtml(string htmlString)
+        {
+            string htmlTagPattern = "<.*?>";
+            var regexCss = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            htmlString = regexCss.Replace(htmlString, string.Empty);
+            htmlString = Regex.Replace(htmlString, htmlTagPattern, string.Empty);
+            htmlString = Regex.Replace(htmlString, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+            htmlString = htmlString.Replace("&nbsp;", string.Empty);
+
+            return htmlString;
+        }
+
     }
 }
